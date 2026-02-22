5fee2f58a537847b9c2293e481d27dc837251ab5
Shivom <shivom19072005@gmail.com>
2026-02-22 22:20:48 +0530

    Fix React hook imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import LoadingScreen from "@/components/LoadingScreen";
 import { Toaster } from "@/components/ui/toaster"
 import { QueryClientProvider } from '@tanstack/react-query'
